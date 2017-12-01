d0c1e2f95d44de271d51664f3387312a2639e498
turtle-insect <turtle.insect@gmail.com>
2017-12-02 02:10:58 +0900

    Add AllGold Param

--- DQ7/DataContext.cs
+++ DQ7/DataContext.cs
@@ -149,6 +149,19 @@ namespace DQ7
 			}
 		}
 
+		public uint AllGold
+		{
+			get
+			{
+				return SaveData.Instance().ReadNumber(0x0540, 4);
+			}
+
+			set
+			{
+				Util.WriteNumber(0x0540, 4, value, 0, 9999999);
+			}
+		}
+
 		public uint BattleCount
 		{
 			get
